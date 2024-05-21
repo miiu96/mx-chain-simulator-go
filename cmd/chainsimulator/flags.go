64a9122d7100cea88c186be196ce8d9b8d8ef809
miiu <mihai9625@gmail.com>
2024-05-21 16:13:16 +0300

    update action

--- cmd/chainsimulator/flags.go
+++ cmd/chainsimulator/flags.go
@@ -127,6 +127,10 @@ var (
 		Name:  "skip-configs-download",
 		Usage: "The flag is used to specify whether to skip downloading configs",
 	}
+	fetchConfigsAndClose = cli.BoolFlag{
+		Name:  "fetch-configs-and-close",
+		Usage: "The flag is used to specify whether to fetch configs",
+	}
 )
 
 func applyFlags(ctx *cli.Context, cfg *config.Config) {
